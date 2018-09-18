16cfd7a1d0d2aefb356aaabad13cf5b771673838
PB16 <marsbar358@gmail.com>
2018-09-18 21:22:20 +0200

    changed setup files

---
+++ setup.sh
@@ -0,0 +1,18 @@
+udo apt-get update && sudo apt-get -y upgrade
+
+sudo apt-get install software-properties-common
+sudo add-apt-repository -y ppa:ethereum/ethereum
+sudo apt-get update
+sudo apt-get install ethereum
+
+sudo apt-get install -y build-essential
+curl -sL https://deb.nodesource.com/setup_10.x | sudo -E bash -
+sudo apt-get install -y nodejs
+
+sudo npm install -g truffle
+
+sudo apt-get install docker-ce
+
+sudo apt-get install docker-compose
+
+sudo docker image build -t testbed .
