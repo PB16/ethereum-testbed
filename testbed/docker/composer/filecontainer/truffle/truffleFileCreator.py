8d7485e66a907e86f4a45cec8191bb43c8ea03bc
PB16 <peterbugtrup97@gmail.com>
2018-10-16 15:14:22 +0200

    Create truffleFileCreator.py

---
+++ testbed/docker/composer/filecontainer/truffle/truffleFileCreator.py
@@ -0,0 +1,25 @@
+import os
+
+dirName = "/home/henrik/Documents/testbed/docker/composer/filecontainer/truffle"
+fileList = os.listdir(dirName)
+for file in fileList:
+  if 'UTC' in file:
+    account = file[-42:-1]
+    print(account)
+
+
+with open("truffle1.js", "wt") as fileOut:
+		fileOut.write(
+"""module.exports = {
+  networks: {
+      development: {
+        host: "172.18.0.100",
+        port: 8545,
+        network_id: "1234", // Match any network id
+        gas: 6500000,
+        gasPrice: 2,
+        from: """ + '"' + account + '"' + """
+      }
+    }
+};"""
+)
