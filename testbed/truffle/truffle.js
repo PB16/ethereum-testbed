dcdeaf22ff69e78734884bf7a5e3c41b4fe6af61
PB16 <peterbugtrup97@gmail.com>
2018-10-16 15:56:38 +0200

    changed dockerfile

--- testbed/truffle/truffle.js
+++ testbed/truffle/truffle.js
@@ -8,7 +8,7 @@ module.exports = {
       host: "localhost",
       port: 8545,
       network_id: "*",
-      from: "0x738f9c800fc80459f8f01c0e685ac2030b4fad2f",
+      from: "",
       gas: 18000000
     }
   }
