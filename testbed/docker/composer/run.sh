2e4eedf3047e6d1c251eceec9cc4581b9043ac10
PB16 <marsbar358@gmail.com>
2018-10-25 18:55:20 +0200

    automatic unlocking of account now works.

--- testbed/docker/composer/run.sh
+++ testbed/docker/composer/run.sh
@@ -1,5 +1,5 @@
 sudo docker-compose up -d
-sleep 1.5
+sleep 10
 sudo docker exec -it composer_miner1_1 /bin/bash -c "accounts.sh"
 sudo docker exec -it composer_miner1_1 /bin/bash -c "python3 setAccount.py"
-sudo docker exec -it composer_miner1_1 /bin/bash -c "geth --datadir miner1 --networkid 1234 --verbosity 6 --unlock \"0xecd8660056afe7a3f0a6b59f90777171b72f4c14\" --password password.txt --mine --minerthreads 1 --rpc --rpcapi \"admin, eth, personal, miner, txpool, net, web3\" 2>> miner1.txt"
+sudo docker exec -it -d composer_miner1_1 /bin/bash -c "geth --datadir workspace/miner1 --networkid 1234 --verbosity 6 --unlock 0 --password password.txt --mine --minerthreads 1 --rpc --rpcapi \"admin, eth, personal, miner, txpool, net, web3\" 2>> workspace/miner1.txt"
