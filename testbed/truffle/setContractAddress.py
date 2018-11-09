94869e97d3d559c992db4290f556e81c53e1b609
PB16 <marsbar358@gmail.com>
2018-11-09 23:15:29 +0100

    adding documentation about the ABI

--- testbed/truffle/setContractAddress.py
+++ testbed/truffle/setContractAddress.py
@@ -11,18 +11,22 @@ with open("/home/peter/ethereum-testbed/testbed/geth/scripts/createTransactions.
 		if not line.lstrip().startswith("//") and "var contractAddress = \"" in line:
 			result = re.search('var contractAddress = \"(.*)\";', line.lstrip())
 
+#extracting the old ABI, so it can be replaced with the new ABI.
 lines = ''.join(lines)
 abi = re.search('eth.contract\((.*)\);', lines, re.DOTALL)
 
+#extracting the new ABI, so the old one can be replaced.
 with open("/home/peter/ethereum-testbed/testbed/truffle/build/contracts/Offer.json") as sfile:
 	lines1 = sfile.read()
 
 result = re.search('\"abi\":(.*),\n  \"bytecode\":', lines1, re.DOTALL)
 
+#replacing old ABI with new ABI, so that it is possible to interact with contract in use.
 with open("/home/peter/ethereum-testbed/testbed/geth/scripts/createTransactions.js") as tfile:
 	lines = tfile.readlines()
 	lines = ''.join(lines)
 	lines = lines.replace(abi.group(1), result.group(1))
 
+#saving the changes.
 with open("/home/peter/ethereum-testbed/testbed/geth/scripts/createTransactions.js","w") as tfile:
 	tfile.writelines(lines)
