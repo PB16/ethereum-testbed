0aa7e7fd23458f5caaf57ee5245cde3aab461480
PB16 <marsbar358@gmail.com>
2018-11-09 21:04:00 +0100

    first version of script for automatic load of test scripts

--- testbed/truffle/setContractAddress.py
+++ testbed/truffle/setContractAddress.py
@@ -8,7 +8,19 @@ with open("migrations.txt", "r") as sfile:
 			print(line.split(':')[1])
 
 with open("/home/peter/ethereum-testbed/testbed/geth/scripts/createTransactions.js") as tfile:
-	for line in tfile:
-		if not line.lstrip().startswith("//") and "var contractAddress =" in line and "\"" in line:
+	lines = tfile.readlines()
+
+	for line in lines:
+		if not line.lstrip().startswith("//") and "var contractAddress = \"" in line:
 			result = re.search('var contractAddress = \"(.*)\";', line.lstrip())
 			print(result.group(1))
+
+lines = ''.join(lines)
+abi = re.search('eth.contract\((.*)\);', lines, re.DOTALL)
+print(abi.group(1))
+
+with open("/home/peter/geth_test/truffle/build/contracts/Offer.json") as sfile:
+	lines1 = sfile.read()
+
+result = re.search('\"abi\":(.*),\n  \"bytecode\":', lines1, re.DOTALL)
+print(result.group(1))
