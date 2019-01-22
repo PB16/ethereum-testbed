9dde2048e567594c0c2b3b9d7bba39e12283d145
PB16 <peterbugtrup97@gmail.com>
2019-01-22 20:21:30 +0100

    automatic deployment of contracts for both miners now work

--- testbed/truffle/setContractAddress.py
+++ testbed/truffle/setContractAddress.py
@@ -5,27 +5,27 @@ newContractAddress = ""
 
 #extracting contract addresses from output of truffle.
 with open("../../truffle/migrations.txt", "r") as sfile:
-	for line in sfile:
-		line = line.replace(" ","")
+    for line in sfile:
+        line = line.replace(" ","")
 
-		if "Offer:" in line:
-			newContractAddress = line.split(':')[1]
+        if "Offer:" in line:
+            newContractAddress = line.split(':')[1]
 
 #extracting the old contract address and replaces with the contract address.
 with open("../../geth/scripts/createTransactions.js") as tfile:
-	lines = tfile.readlines()
+    lines = tfile.readlines()
 
-	for line in lines:
-		if not line.lstrip().startswith("//") and "var contractAddress = \"" in line:
-			result = re.search('var contractAddress = \"(.*)\";', line.lstrip())
-			oldContractAdress = result.group(1)
-			lines = ''.join(lines)
-			lines = lines.replace(oldContractAdress, newContractAddress.strip("\n"))
-			break
+    for line in lines:
+        if not line.lstrip().startswith("//") and "var contractAddress = \"" in line:
+            result = re.search('var contractAddress = \"(.*)\";', line.lstrip())
+            oldContractAdress = result.group(1)
+            lines = ''.join(lines)
+            lines = lines.replace(oldContractAdress, newContractAddress.strip("\n"))
+            break
 
 #saving the changes.
 with open("../../geth/scripts/createTransactions.js","w") as tfile:
-	tfile.writelines(lines)
+    tfile.writelines(lines)
 
 #extracting the old ABI, so it can be replaced with the new ABI.
 lines = ''.join(lines)
@@ -33,18 +33,18 @@ abi = re.search('eth.contract\((.*)\);', lines, re.DOTALL)
 
 #extracting the new ABI, so the old one can be replaced.
 with open("../../truffle/build/contracts/Offer.json") as sfile:
-	lines1 = sfile.read()
+    lines1 = sfile.read()
 
 result = re.search('\"abi\":(.*),\n  \"bytecode\":', lines1, re.DOTALL)
 
 #replacing old ABI with new ABI, so that it is possible to interact with contract in use.
 with open("../../geth/scripts/createTransactions.js") as tfile:
-	lines = tfile.readlines()
-	lines = ''.join(lines)
-	lines = lines.replace(abi.group(1), result.group(1))
+    lines = tfile.readlines()
+    lines = ''.join(lines)
+    lines = lines.replace(abi.group(1), result.group(1))
 
 #saving the changes.
 with open("../../geth/scripts/createTransactions.js","w") as tfile:
-	tfile.writelines(lines)
+    tfile.writelines(lines)
 
 print("done")
