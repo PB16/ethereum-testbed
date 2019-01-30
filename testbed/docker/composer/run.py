fa27e8e292b70849e6cd6460b68d05faec78d054
PB16 <peterbugtrup97@gmail.com>
2019-01-30 11:17:29 +0100

    multiple miners are now supported

--- testbed/docker/composer/run.py
+++ testbed/docker/composer/run.py
@@ -9,8 +9,37 @@ dockerStartCmd = "sudo docker-compose up -d"
 
 subprocess.call(dockerStartCmd.split())
 
+sleep(5)
+
 for x in next(os.walk(minerspath))[1]:
     if "miner" in x:
         miners.append(x)
 
-print(miners)
+sleep(5)
+
+for x in miners:
+    subprocess.call(['sudo','./accounts.sh',x])
+
+sleep(5)
+
+for x in miners:
+    subprocess.call(['sudo','python3','../../truffle/automatic-migration.py',x])
+
+sleep(5)
+
+for x in miners:
+    subprocess.call(['sudo','./migration.sh',x])
+
+sleep(5)
+
+for x in miners:
+    subprocess.call(['sudo','python3','../../truffle/setContractAddress.py',x])
+
+sleep(5)
+
+for x in miners:
+    subprocess.call(['sudo','python3','../../truffle/load-test-scripts.py',x])
+
+sleep(5)
+
+print("testbed has succesfully started!")
--- testbed/truffle/setContractAddress.py
+++ testbed/truffle/setContractAddress.py
@@ -1,50 +1,62 @@
 import re
 import os
+import sys
+
+migrationsfile = sys.argv[1] + "_migrations.txt"
 
 newContractAddress = ""
+i = 0
 
 #extracting contract addresses from output of truffle.
-with open("../../truffle/migrations.txt", "r") as sfile:
-	for line in sfile:
-		line = line.replace(" ","")
+with open("../../truffle/" + migrationsfile, "r") as sfile:
+    lines = sfile.readlines()
+    for line in lines:
+        line = line.replace(" ","")
 
-		if "Offer:" in line:
-			newContractAddress = line.split(':')[1]
+        if "Deploying'subscription'" in line:
+            for line in lines[i:]:
+                line = line.replace(" ","")
+                
+                if 'contractaddress:' in line:
+                    newContractAddress = line.split(':')[1]
+                    break
+
+        i += 1
 
 #extracting the old contract address and replaces with the contract address.
 with open("../../geth/scripts/createTransactions.js") as tfile:
-	lines = tfile.readlines()
+    lines = tfile.readlines()
 
-	for line in lines:
-		if not line.lstrip().startswith("//") and "var contractAddress = \"" in line:
-			result = re.search('var contractAddress = \"(.*)\";', line.lstrip())
-			oldContractAdress = result.group(1)
-			lines = ''.join(lines)
-			lines = lines.replace(oldContractAdress, newContractAddress.strip("\n"))
-			break
+    for line in lines:
+        if not line.lstrip().startswith("//") and "var contractAddress = \"" in line:
+            result = re.search('var contractAddress = \"(.*)\";', line.lstrip())
+            oldContractAdress = result.group(1)
+            lines = ''.join(lines)
+            lines = lines.replace(oldContractAdress, newContractAddress.strip("\n"))
+            break
 
 #saving the changes.
 with open("../../geth/scripts/createTransactions.js","w") as tfile:
-	tfile.writelines(lines)
+    tfile.writelines(lines)
 
 #extracting the old ABI, so it can be replaced with the new ABI.
 lines = ''.join(lines)
 abi = re.search('eth.contract\((.*)\);', lines, re.DOTALL)
 
 #extracting the new ABI, so the old one can be replaced.
-with open("../../truffle/build/contracts/Offer.json") as sfile:
-	lines1 = sfile.read()
+with open("../../truffle/build/contracts/subscription.json") as sfile:
+    lines1 = sfile.read()
 
 result = re.search('\"abi\":(.*),\n  \"bytecode\":', lines1, re.DOTALL)
 
 #replacing old ABI with new ABI, so that it is possible to interact with contract in use.
 with open("../../geth/scripts/createTransactions.js") as tfile:
-	lines = tfile.readlines()
-	lines = ''.join(lines)
-	lines = lines.replace(abi.group(1), result.group(1))
+    lines = tfile.readlines()
+    lines = ''.join(lines)
+    lines = lines.replace(abi.group(1), result.group(1))
 
 #saving the changes.
 with open("../../geth/scripts/createTransactions.js","w") as tfile:
-	tfile.writelines(lines)
+    tfile.writelines(lines)
 
 print("done")
